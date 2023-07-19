9827ca4e8abd577c19bfe9fa8025c7aaa33fffe7
Florian Sandel <florian@sandel.email>
2023-07-19 19:46:44 +0200

    wordpress stays running

--- srcs/requirements/wordpress/tools/wordpress_setup.sh
+++ srcs/requirements/wordpress/tools/wordpress_setup.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
 mkdir -p /var/www/html
-
 cd /var/www/html
-
 rm -rf /var/www/html/*
 
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
-
-chmod +x wp-cli.phar
-
-mv wp-cli.phar /usr/local/bin/wp
-
-wp core download --allow-root
+wget http://wordpress.org/latest.tar.gz
+tar xfz latest.tar.gz
+mv wordpress/* .
+rm -rf latest.tar.gz
+rm -rf wordpress
 
 mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-/bin/bash
+
+php -S 0.0.0.0:9000 -t .
